dae21305eb7c551863ac351a1d688cd8a5f39e4d
pablogch <pablochoconi@gmail.com>
2023-06-18 19:26:50 -0300

    New mapping for scrolling completion suggestions

--- lua/nvim-cmp-config.lua
+++ lua/nvim-cmp-config.lua
@@ -16,6 +16,8 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item()
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },

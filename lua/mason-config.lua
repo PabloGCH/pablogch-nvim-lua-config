545ac5e9e88896e3295c34fd4b7f8bf0841c8c4e
pablogch <pablochoconi@gmail.com>
2023-06-19 17:16:45 -0300

    goto definition fix

--- lua/mason-config.lua
+++ lua/mason-config.lua
@@ -2,7 +2,7 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 local caps = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local on_attach = function(client, bufnr)
-    local opts = { buffer = ev.buf }
+    local opts = { buffer = bufnr, noremap = true, silent = true }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
